399502a1a86137de9ecbf993f604487c0eebc49e
Marina Alane <marina.alane@outlook.com>
2021-11-03 16:33:12 -0300

    cria rotas

---
+++ backEnd/Routes/todoRoutes.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+router.get("/list", (_req, res) => {
+  res.send()
+});
+
+router.get("/add", (_req, res) => {
+  res.send()
+});
+
+router.get("/update", (_req, res) => {
+  res.send()
+});
+
+router.get("/delete", (_req, res) => {
+  res.send()
+});
+
+ 
+ module.exports = router;
